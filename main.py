d4694dc6943324944278848f3b0dda8684ec3e34
callmeray16 <100089236+callmeray16@users.noreply.github.com>
2026-03-13 17:07:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+#uvicorn main:app --reload
+# 2. Create your 'App' (This is your Kitchen)
+app = FastAPI()
+
+@app.get("/")
+def home_page():
+    # 4. Define the 'Response' (The Goods)
+    return "This is very interesting"
